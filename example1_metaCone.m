aad5ab45d898b1be053e5f58b7697c1b9ba24547
alvinius9 <118293143+alvinius9@users.noreply.github.com>
2025-03-17 12:41:49 -0300

    Update and rename main.m to example1_metaCone.m

---
+++ example1_metaCone.m
@@ -0,0 +1,8 @@
+%% 
+initCobraToolbox(false)
+changeCobrasolver('gurobi','ALL');
+
+%% LOADING MODELS
+% Run this section from the main repository folder
+cd ./models
+
--- main.m
+++
@@ -1,8 +0,0 @@
-%% Run this file by section to get the results one by one. 
-initCobraToolbox(false)
-
-%% Section 1
-
-%% Section 2
-
-%% Section 3
