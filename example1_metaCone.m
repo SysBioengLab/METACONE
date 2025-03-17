1c026781cf2fe1f829bf492a15d02b3cd2ff2449
alvinius9 <118293143+alvinius9@users.noreply.github.com>
2025-03-17 16:53:25 -0300

    Update example1_metaCone.m

--- example1_metaCone.m
+++ example1_metaCone.m
@@ -1,8 +1,18 @@
 %% 
+clc; 
 initCobraToolbox(false)
 changeCobrasolver('gurobi','ALL');
 
 %% LOADING MODELS
 % Run this section from the main repository folder
-cd ./models
 
+load('./models/toymodel1_bio.mat')
+load('./models/bmodel.mat')
+load('./models/iML1515.mat')
+load('./models/PD.mat'); PD = model;
+load('./models/LS.mat'); LS = model;
+load('./models/yeast-GEM.mat'); yeast8 = model; clear model
+
+%% SETTING PARAMETERS
+% metaCone can be run with several different parameters.
+% Make sure you call the function according to your needs.
